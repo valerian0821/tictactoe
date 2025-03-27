470235cbf928aa1b8f7e4811879c27818e773210
Valerian Ha <ha.valerian@gmail.com>
2025-03-27 19:20:31 -0400

    Finish gamecontroller

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ const GameBoard = (function () {
         for (let i = 0; i < board.length; i++) {
             console.log(board[i]);
         }
+        console.log("");
     }
 
     const checkForWinner = () => {
@@ -75,17 +76,86 @@ const GameBoard = (function () {
     }
 })();
 
-GameBoard.getBoard();
-console.log(GameBoard.checkAvailability(0, 0));
-GameBoard.placeMarker("O", 0, 0);
-GameBoard.placeMarker("X", 0, 1);
-GameBoard.placeMarker("O", 1, 1);
-GameBoard.placeMarker("X", 0, 2);
-GameBoard.placeMarker("O", 2, 0);
-GameBoard.placeMarker("X", 1, 0);
-GameBoard.placeMarker("O", 2, 1);
-GameBoard.placeMarker("X", 2, 2);
-GameBoard.placeMarker("O", 1, 2);
-GameBoard.printBoard();
-console.log(GameBoard.checkForWinner());
+function createPlayer(name, marker) {
+    return {name, marker};
+}
+
+function switchActiveMarker(marker) {
+    let newMarker;
+    if (marker === player1.marker) {
+        newMarker = player2.marker;
+    } else {
+        newMarker = player1.marker;
+    }
+    return newMarker;
+}
+
+function switchActivePlayer(activePlayer) {
+    let newActivePlayer;
+    if (activePlayer === player1.name) {
+        newActivePlayer = player2.name;
+    } else {
+       newActivePlayer = player1.name;
+    }
+    return newActivePlayer;
+}
+
+function announceWinner(marker) {
+    let winningPlayer;
+    if (marker === player1.marker) {
+        winningPlayer = player1.name;
+    } else {
+        winningPlayer = player2.name;
+    }
+    console.log(`${winningPlayer} has won the game!`);
+}
+
+function gameController() {
+    GameBoard.getBoard();
+    let winnerFound = false;
+    let numOfTurns = 0;
+    let activeMarker = player1.marker;
+    let activePlayer = player1.name
+    while (winnerFound === false && numOfTurns < 9) {
+        console.log(`It is ${activePlayer}'s turn.`);
+        let rowInput = prompt("Enter Row Number: ");
+        let columnInput = prompt("Enter Column Number: ");
+        while (GameBoard.checkAvailability(rowInput, columnInput) === false) {
+            rowInput = prompt("Invalid. Re-enter Row Number: ");
+            columnInput = prompt("Re-enter Column Number: ")
+        }
+        GameBoard.placeMarker(activeMarker, rowInput, columnInput);
+        GameBoard.printBoard();
+        numOfTurns++;
+
+        //Cannot win in tic-tac-toe until at least the 5th turn (First player's third turn)
+        if (numOfTurns >= 5) {
+            winnerFound = GameBoard.checkForWinner();
+            if (winnerFound) {
+                announceWinner(activeMarker);
+            }
+        }
+        activeMarker = switchActiveMarker(activeMarker);
+        activePlayer = switchActivePlayer(activePlayer);
+    }
+    if (winnerFound === false) {
+        console.log("This game is a tie!");
+    }
+}
+const player1 = createPlayer("Player 1", "X");
+const player2 = createPlayer("Player 2", "O");
+gameController();
+// GameBoard.getBoard();
+// console.log(GameBoard.checkAvailability(0, 0));
+// GameBoard.placeMarker("O", 0, 0);
+// GameBoard.placeMarker("X", 0, 1);
+// GameBoard.placeMarker("O", 1, 1);
+// GameBoard.placeMarker("X", 0, 2);
+// GameBoard.placeMarker("O", 2, 0);
+// GameBoard.placeMarker("X", 1, 0);
+// GameBoard.placeMarker("O", 2, 1);
+// GameBoard.placeMarker("X", 2, 2);
+// GameBoard.placeMarker("O", 1, 2);
+// GameBoard.printBoard();
+// console.log(GameBoard.checkForWinner());
 
